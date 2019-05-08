c5a9fe38ecd694d157b0f61485d7fb616a03bc6d
liangjialin <smile_raccoon@126.com>
2019-05-08 14:17:02 +0800

    修改布局文字

--- blog/views.py
+++ blog/views.py
@@ -13,12 +13,8 @@ def yd_information(request):
     :param request:
     :return:
     """
-    # context = {}
-    # # data = db.default.mobile_subject_detail_hn.filter(subject_id=2)
-    # args = request.QUERY.casts(page=int)
     if request.method == 'GET':
         page = int(request.GET.get('page',1))
-        # page = args.page or 1
         out = Struct()
         sql_cycle_info = '''
             select id,cycle_max_id,main_cycle_num,add_time,sql_cycle_num from sql_cycle_info;
@@ -29,12 +25,10 @@ def yd_information(request):
             data.add_time = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
             out.data_info = data
             out.compare_num = data.sql_cycle_num * 500
-        #     return render_template(request, 'login.html',context)
         total = yd_information_common()    # 返回总页数
         num_total = math.ceil(total / 10)
         data = error_info(page, 10)
         data, page_range = api_page(data, page, total)
-
         out.data = data
         out.page_range = page_range
         out.page = page
