ea4f896f96ae0a92148060917ca2eb777fd8a8cf
liangjialin <smile_raccoon@126.com>
2019-05-15 11:07:25 +0800

    修改分页bug

--- blog/common/views.py
+++ blog/common/views.py
@@ -12,6 +12,6 @@ def yd_information_common():
 def error_info(page, page_size, cycle_num):
     error_info_sql = '''
         select id,phone_num,code,ecid,subject_id,after_status,cycle_num from tbkt_base.mobile_cycle_exception where cycle_num = %s limit %s,%s;
-    '''% (cycle_num, (page - 1)*page_size, page_size * page)
+    '''% (cycle_num, (page - 1)*page_size, page_size)
     data = db.yd.fetchall_dict(error_info_sql)
     return data
